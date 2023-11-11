81fb477c4a44b6fe7be26793a078c2bfe4994c7a
KamaL145 <niyaz.kamalov.21@gmail.com>
2023-11-12 00:10:56 +0300

    added: urls for equipment management api

--- equip_manager/urls.py
+++ equip_manager/urls.py
@@ -16,17 +16,40 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
 
 from web.serializers import MyTokenObtainPairSerializer
+from web.views import EquipmentManagement
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path(
         "api/user/login/",
         TokenObtainPairView.as_view(serializer_class=MyTokenObtainPairSerializer),
         name="token_obtain_pair",
     ),
+    path("api/equipment/", EquipmentManagement.as_view(), name="equipment_management")
 ]
