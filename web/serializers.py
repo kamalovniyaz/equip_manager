f7ed4ff50282e5902fe9b04b7ec082ab480dfee2
KamaL145 <niyaz.kamalov.21@gmail.com>
2023-11-12 00:10:05 +0300

    added: equipment serializer validate serial numbers and equipment creation method

--- web/serializers.py
+++ web/serializers.py
@@ -1,8 +1,16 @@
+import re
+
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from web.models import Equipment, EquipmentType
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Переопределенный метод для проверки пользователя и выдачи ему JWT токена
+    """
+
     username_field = "email"
 
     def validate(self, attrs):
@@ -25,3 +33,49 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         data["refresh"] = str(refresh)
         data["access"] = str(refresh.access_token)
         return data
+
+
+class EquipmentSerializer(serializers.Serializer):
+    """
+    Сериализация для модели оборудования
+    """
+
+    class Meta:
+        model = Equipment
+        fields = "__all__"
+
+    def create(self, validated_data):
+        Equipment.objects.create(equipment_type=validated_data.get('equipment_type'),
+                                             serial_number=validated_data.get('serial_number'))
+        return True
+
+    def validate_serial_numbers(self):
+        """
+        Метод для проверки валидности маски оборудования.
+        :return: True or ValidationError
+        """
+        # Получаем данные оборудования из входных данных
+        equipment_type = self.initial_data.get('equipment_type')
+        serial_number = self.initial_data.get('serial_number')
+
+        # Получаем валидную маску для этого типа оборудования
+        try:
+            equip_type = EquipmentType.objects.get(name=equipment_type)
+        except EquipmentType.DoesNotExist:
+            raise serializers.ValidationError("Неподдерживаемый тип оборудования")
+
+        # Создание регулярного выражения для каждой позиции в маске
+        regex_patterns = {
+            'N': r'\d',  # Цифра от 0 до 9
+            'A': r'[A-Z]',  # Прописная буква латинского алфавита
+            'a': r'[a-z]',  # Строчная буква латинского алфавита
+            'X': r'[A-Z\d]',  # Прописная буква латинского алфавита или цифра от 0 до 9
+            'Z': r'[-_@]'  # Символ из списка: "-", "_", "@"
+        }
+
+        # Проверка серийного номера с помощью соответствующей маски
+        if re.match('^' + ''.join([regex_patterns.get(char, '.') for char in equip_type.serial_number_mask]) + '$',
+                    serial_number):
+            return True
+        else:
+            raise serializers.ValidationError(f"Серийный номер {serial_number} не соответствует маске")
