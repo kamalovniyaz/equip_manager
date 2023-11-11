4e8f127eb374d0fd57e244969f8dce920fde9015
KamaL145 <niyaz.kamalov.21@gmail.com>
2023-11-12 00:07:49 +0300

    updated: unique fields

--- web/models.py
+++ web/models.py
@@ -12,9 +12,14 @@ class EquipmentType(models.Model):
 
 class Equipment(models.Model):
     id = models.AutoField(primary_key=True)
-    equipment_type = models.ForeignKey(EquipmentType, verbose_name='Тип оборудования', on_delete=models.CASCADE)
+    equipment_type = models.CharField(verbose_name='Тип оборудования', max_length=50)
     serial_number = models.CharField(verbose_name='Серийный номер', max_length=50, unique=True)
+    is_active = models.BooleanField(verbose_name='Активно', default=True)
     note = models.TextField(verbose_name='Примечание')
+
+    class Meta:
+        unique_together = ('equipment_type', 'serial_number') # Связка уникальных полей
 
     def __str__(self):
         return self.serial_number
+
