8bc3ed0a35ca2c910d4913b910eaf0a7bac957aa
KamaL145 <niyaz.kamalov.21@gmail.com>
2023-11-13 00:04:29 +0300

    updated: h2 label

--- frontend/equip_frontend/src/components/EquipmentEdit.vue
+++ frontend/equip_frontend/src/components/EquipmentEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="centered-container">
-    <h2>Добавление оборудования</h2>
+    <h2>Изменения данных оборудования</h2>
   </div>
   <div class="new-equipment-add">
     <div class="form-columns">
