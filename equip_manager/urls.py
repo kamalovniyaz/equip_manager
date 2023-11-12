4f08558137b6ed67c0ad1840d2f09169403ae57b
KamaL145 <niyaz.kamalov.21@gmail.com>
2023-11-12 23:26:25 +0300

    added: main url

--- equip_manager/urls.py
+++ equip_manager/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -45,6 +46,10 @@ urlpatterns = [
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^(?!(api|admin|swagger)/).*$",
+        TemplateView.as_view(template_name="main.html"),
     ),
     path(
         "api/user/login/",
